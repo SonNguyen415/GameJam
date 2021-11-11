ac91d4f854c6749a9c548dde1422e6c4f4c9fa10
Son Nguyen <Son.mofa415@gmail.com>
2021-11-10 20:24:23 -0500

    Character Movement

--- objects.py
+++ objects.py
@@ -1,18 +1,48 @@
 import time
 import pygame
 
-class Character(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([5, 5])
-        self.image.fill('red')
+WHITE = (255, 255, 255)
+
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, xLoc, yLoc):
+         # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Load the image
+        self.image = pygame.image.load("player.png")
+    
+        # Set our transparent color
+        self.image.set_colorkey(WHITE)
+
+        # Some character data
         self.health = 100
         self.recharge = 3
+        self.xLoc = xLoc
+        self.yLoc = yLoc
+        self.movementSpeed = 5
         self.stamina = 20
-        self.bmrUse = True
+        self.usingBMR = True
+
+    def handle_keys(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_DOWN]: 
+            self.yLoc += self.movementSpeed 
+        elif key[pygame.K_UP]:
+            self.yLoc -= self.movementSpeed
+        if key[pygame.K_RIGHT]: 
+            self.xLoc += self.movementSpeed 
+        elif key[pygame.K_LEFT]: 
+            self.xLoc -= self.movementSpeed 
+        
     
+    def draw(self, surface):
+        # blit yourself at your current position
+        surface.blit(self.image, (self.xLoc, self.yLoc))
+
     def sprint(self):
+        self.movementSpeed = 7
         while(self.stamina > 0):
             self.stamina -= 1
             time.sleep(0.1)
@@ -20,7 +50,8 @@ class Character(pygame.sprite.Sprite):
     def wounded(self):
         self.health -= 10
 
+    # def throw_boomerang(self, boomerang):
+        # Boomerang = instance of boomerang
+        # Boomerang.spawn_boomerang()
 
-    
 
-        
