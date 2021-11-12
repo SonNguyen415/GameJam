c707bf42a6fbdbf0487011b47b8bda00fd7b97e8
OliverKris <93892018+OliverKris@users.noreply.github.com>
2021-11-11 21:33:56 -0500

    Update button_main.py

--- button_main.py
+++ button_main.py
@@ -13,16 +13,21 @@ pygame.display.set_caption("Jelly Jammers")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
-
 # Set image path as variables for later use
 start_img = pygame.image.load("Button Icons/Play Button.png").convert_alpha()
 exit_img = pygame.image.load("Button Icons/Quit Button.png").convert_alpha()
+credit_img = pygame.image.load("Button Icons/Credit Button.png").convert_alpha()
 bg = pygame.image.load("Background.png")
 
-startButton = button.Button(100, 200, start_img, 10)
-exitButton = button.Button(500, 200, exit_img, 10)
+width = screen.get_width()
+height = screen.get_height()
+startButtonWidth = start_img.get_width()
+exitButtonWidth = exit_img.get_width()
+creditButtonWidth = credit_img.get_width()
 
-
+startButton = button.Button(((width/2) - (startButtonWidth*10/2)), 100, start_img, 10)
+exitButton = button.Button(((width/2) - (exitButtonWidth*10/2)), 400, exit_img, 10)
+creditButton = button.Button(((width/2) - (creditButtonWidth*10/2)), 250, credit_img, 10)
 
 running = True
 while running:
@@ -40,6 +45,8 @@ while running:
         print("Start Button Pressed")
     if exitButton.draw(screen):
         running = False
+    if creditButton.draw(screen):
+        print("Credit Button Pressed")
 
     pygame.display.update()
 
