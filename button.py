4ae275f08f9072155e046e216e8083c5d8a3f490
OliverKris <93892018+OliverKris@users.noreply.github.com>
2021-11-11 20:21:28 -0500

    Update button.py

--- button.py
+++ button.py
@@ -15,7 +15,8 @@ pygame.display.set_icon(icon)
 
 start_img = pygame.image.load("Button Icons/Play Button.png").convert_alpha()
 exit_img = pygame.image.load("Button Icons/Quit Button.png").convert_alpha()
-
+start_img = pygame.transform.scale(start_img, (310, 110))
+exit_img = pygame.transform.scale(exit_img, (310, 110))
 
 class Button():
     def __init__(self, x, y , image):
@@ -26,12 +27,13 @@ class Button():
        self.rect = self.image.get_rect()
        self.rect.topleft = (x,y)
 
-    def draw():
+    def draw(self):
         # Draw button on the screen
-        screen.blit(self.image (self.rect.x, self.rect.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
 startButton = Button(100, 200, start_img)
 exitButton = Button(500, 200, exit_img)
+
 
 
 running = True
@@ -40,8 +42,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            runnning = False
+            runnning  = False
 
     screen.fill((202, 228, 241))
+
+    startButton.draw()
+    exitButton.draw()
 
     pygame.display.update()
