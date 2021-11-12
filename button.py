6c6600448218315e87f2f8e2e800cb5878363cbf
OliverKris <93892018+OliverKris@users.noreply.github.com>
2021-11-11 20:56:14 -0500

    Button Class Almost Finished

--- button.py
+++ button.py
@@ -13,9 +13,11 @@ icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 
+# Set image path as variables for later use
 start_img = pygame.image.load("Button Icons/Play Button.png").convert_alpha()
 exit_img = pygame.image.load("Button Icons/Quit Button.png").convert_alpha()
 
+# Create a button class to apply to multiple buttons
 class Button():
     def __init__(self, x, y , image, scale):
         # Call the parent class (Sprite) constructor
@@ -25,10 +27,29 @@ class Button():
        self.image = pygame.transform.scale(image, (int(width*scale),int(height*scale)))
        self.rect = self.image.get_rect()
        self.rect.topleft = (x,y)
+       self.clicked = False
 
     def draw(self):
+
+        action = False
+        pos = pygame.mouse.get_pos()
+
+        # Conditional statement to check if the button is pressed
+        if self.rect.collidepoint(pos):
+             if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                 self.clicked = True
+                 action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
         # Draw button on the screen
         screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        # Returns if the button is pressed
+        return action
+
+
 
 startButton = Button(100, 200, start_img, 10)
 exitButton = Button(500, 200, exit_img, 10)
@@ -37,15 +58,20 @@ exitButton = Button(500, 200, exit_img, 10)
 
 running = True
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            runnning  = False
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     screen.fill((202, 228, 241))
 
-    startButton.draw()
-    exitButton.draw()
+    if startButton.draw():
+        print("Start Button Pressed")
+    if exitButton.draw():
+        running = False
 
     pygame.display.update()
+
+pygame.quit()
