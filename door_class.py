dad4b13f55a3ceb3c411ed561631bb5db1661a0c
OliverKris <93892018+OliverKris@users.noreply.github.com>
2021-11-17 23:00:12 -0500

    door class

--- door_class.py
+++ door_class.py
@@ -2,12 +2,17 @@ import pygame
 from settings import *
 import time
 
+playerPosition = [2,2]
+
+
 class Door(pygame.sprite.Sprite):
     def __init__(self, xLoc, yLoc, charImg, rotation):
         super().__init__()
 
         self.xLoc = xLoc
         self.yLoc = yLoc
+
+        self.state = 'locked'
 
         self.sprite = charImg
 
@@ -18,3 +23,14 @@ class Door(pygame.sprite.Sprite):
     def draw(self, surface):
         self.sprite = pygame.transform.scale(self.image, (CHAR_WIDTH, CHAR_HEIGHT))
         surface.blit(self.sprite, (xLoc, yLoc))
+
+    def change_position(self):
+        if rotation == 'N':
+            playerPosition[1] -= 1
+        elif rotation == 'S':
+            playerPosition[1] += 1
+        elif rotation == 'W':
+            playerPosition[0] -= 1
+        elif rotation == 'E':
+            playerPosition[0] += 1
+        
