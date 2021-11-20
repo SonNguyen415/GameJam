0e49b5d173e6cc1c36cda4aee14a07b19f55bba3
Warnerd <94016157+Warnerd03@users.noreply.github.com>
2021-11-20 17:24:16 -0500

    N

--- classes.py
+++ classes.py
@@ -99,14 +99,28 @@ class Playground():
 
 
     def generate_enemies(self):
-        numEnemy = 1
+        numEnemy = random.randint(1,6)
         for i in range(1, numEnemy+1):
-            npc = Enemy(500-(numEnemy*60)+100*i, 100, NPC_IMG, i)
+            npc = Enemy(random.randint(PLAYGROUND_X_OFFSET, WINDOW_LENGTH-PLAYGROUND_X_OFFSET-CHAR_WIDTH), random.randint(PLAYGROUND_Y_OFFSET, WINDOW_HEIGHT-PLAYGROUND_Y_OFFSET-CHAR_HEIGHT), NPC_IMG, i)
             self.spriteList.append(npc)
+
+    def generate_obstacles(self):
+        numRock = random.randint(1, 8)
+        for i in range(1, numRock + 1):
+            rock = SpriteObject(random.randint(PLAYGROUND_X_OFFSET, WINDOW_LENGTH - PLAYGROUND_X_OFFSET - ROCK_SIZE), random.randint(PLAYGROUND_Y_OFFSET, WINDOW_HEIGHT - PLAYGROUND_Y_OFFSET - ROCK_SIZE), ROCK_IMG, ROCK_SIZE,'rock')
+            self.spriteList.append(rock)
+        numWall = random.randint(1,6)
+        for i in range(1, numWall + 1):
+            randX = random.randint(PLAYGROUND_X_OFFSET, WINDOW_LENGTH - PLAYGROUND_X_OFFSET - WALL_SIZE*3)
+            randY = random.randint(PLAYGROUND_Y_OFFSET, WINDOW_HEIGHT - PLAYGROUND_Y_OFFSET - WALL_SIZE*3)
+            for j in range(0,3):
+                wall = SpriteObject(randX + WALL_SIZE*j, randY, WALL_IMG, WALL_SIZE,'wall')
+                self.spriteList.append(wall)
 
     def generate_sprites(self):
         self.initiate_doors()
         self.generate_enemies()
+        self.generate_obstacles()
 
     def updateMap(self):
         while len(self.spriteList) > 1:
