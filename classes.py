093dc51695cce4cb4856fc5e085d2d65548e7ce7
Warnerd <94016157+Warnerd03@users.noreply.github.com>
2021-11-20 16:35:14 -0500

    n

--- classes.py
+++ classes.py
@@ -266,19 +266,15 @@ class Character(pygame.sprite.Sprite):
                         if self.yLoc > WINDOW_HEIGHT*(7/10):
                             self.xLoc = WINDOW_LENGTH / 2
                             self.yLoc = 120
-                        if self.yLoc < WINDOW_HEIGHT/5:
+                        elif self.yLoc < WINDOW_HEIGHT/5:
                             self.xLoc = WINDOW_LENGTH / 2
                             self.yLoc = PLAYGROUND_HEIGHT
-                        if self.xLoc < WINDOW_LENGTH/5:
+                        elif self.xLoc < WINDOW_LENGTH/5:
                             self.yLoc = WINDOW_HEIGHT / 2
                             self.xLoc = PLAYGROUND_LENGTH+100
-                        if self.xLoc > WINDOW_LENGTH*(4/5):
+                        elif self.xLoc > WINDOW_LENGTH*(4/5):
                             self.yLoc = WINDOW_HEIGHT / 2
                             self.xLoc = PLAYGROUND_X_OFFSET
-
-                        # Change player position here
-                        self.xLoc = WINDOW_LENGTH/2
-                        self.yLoc = PLAYGROUND_HEIGHT
                         time.sleep(0.1)
                         return
                     if(eachSprite.type == "player" and self.type == "npc"):
